3bb7ca32b4bee407cd443f9dcbf2fc4713713b10
Jinkyou Son <nallwhy@gmail.com>
2023-05-21 18:44:19 +0900

    Cleanup Doumi.Port

--- lib/doumi/port.ex
+++ lib/doumi/port.ex
@@ -1,18 +1,2 @@
 defmodule Doumi.Port do
-  @moduledoc """
-  Documentation for `Doumi.Port`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Doumi.Port.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/doumi/port_test.exs
+++ test/doumi/port_test.exs
@@ -1,8 +1,3 @@
 defmodule Doumi.PortTest do
   use ExUnit.Case
-  doctest Doumi.Port
-
-  test "greets the world" do
-    assert Doumi.Port.hello() == :world
-  end
 end
