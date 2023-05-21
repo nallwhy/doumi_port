2ae3d0b014108122af966458ac7f0708045004ab
Jinkyou Son <nallwhy@gmail.com>
2023-05-21 21:26:07 +0900

    Create Doumi.Port behaviour

--- lib/doumi/port.ex
+++ lib/doumi/port.ex
@@ -1,2 +1,18 @@
 defmodule Doumi.Port do
+  @type name() :: {:global, atom()} | {:via, atom(), atom()} | atom()
+  @type instance() :: pid() | name()
+  @type start_result() :: {:ok, pid()} | {:error, term}
+
+  @callback start(opts :: list()) :: start_result()
+  @callback start(name :: name(), opts :: list()) :: start_result()
+  @callback start_link(opts :: list()) :: start_result()
+  @callback start_link(name :: name(), opts :: list()) :: start_result()
+  @callback call(
+              instance :: instance(),
+              module :: atom(),
+              fun :: atom(),
+              args :: list(),
+              opts :: list()
+            ) :: term()
+  @callback stop(instance :: instance()) :: :ok
 end
